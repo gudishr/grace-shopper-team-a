3060f7c23fef9f187037d4331b0f0ae43f99c32d
shruti gudi <shrutigudi@gmail.com>
2019-11-04 21:23:53 -0800

    set force to false

--- db.js
+++ db.js
@@ -189,7 +189,7 @@ Product.hasMany(Lineitem);
 Lineitem.belongsTo(Product);
 
 const sync = async () => {
-  await conn.sync({ force: true });
+  await conn.sync({ force: false });
   let users = [
     {name: 'Shruti', email: 'shruti@email.com', password: 'SHRUTI'},
     {name: 'Akshay', email: 'akshay@email.com', password: 'AKSHAY'},
